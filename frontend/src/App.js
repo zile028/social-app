6034879aec5affbe1e4f311686e5c7c92ced27f6
zile028 <zile028@gmail.com>
2024-06-29 20:47:06 +0200

    react refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [firstName, setFirstName] = useState();
+    // const [lastName, setLastName] = useState();
+    const [fullName, setFullName] = useState({
+        firstName: "", lastName: ""
+    });
+    const [names, setNames] = useState([]);
+    const [timer, setTimer] = useState(0);
+
+    useEffect(() => {
+
+        console.log("Mount");
+        console.log("Update");
+        return () => {
+            
+        };
+    }, [fullName.firstName]);
+
+
+    const showNames = (e) => {
+        e.preventDefault();
+        console.log(fullName);
+        setNames([...names, fullName]);
+        setFullName({
+            firstName: "", lastName: ""
+        });
+    };
+
+    const inputHandler = (e) => {
+        // let copyFullName = {...fullName};
+        // copyFullName[e.target.name] = e.target.value;
+        // setFullName(copyFullName);
+        setFullName({...fullName, [e.target.name]: e.target.value});
+    };
+
+    return (
+        <div className="App">
+            <p>Hello App</p>
+            <form action="">
+                <input
+                    type="text"
+                    name="firstName"
+                    placeholder="First name"
+                    onInput={inputHandler} value={fullName.firstName}/>
+                <input type="text" name="lastName" placeholder="Last name"
+                       onInput={inputHandler} value={fullName.lastName}/>
+                <button onClick={showNames}>Click</button>
+            </form>
+            {names.map((name, index) => {
+                return <p key={index}>{name.firstName} {name.lastName}</p>;
+            })}
+
+        </div>
+    );
 }
 
 export default App;
